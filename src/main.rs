9d893ad410fec8a067f08b23978d8a6fa7368f1b
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-05 15:23:28 +0200

    Update crate for sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use sqlite::Connection;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
@@ -8,6 +7,11 @@ const ACTORS_TSV_FILE: &str = "name.basics.tsv";
 
 const DATABASE_NAME: &str = "imdb.db";
 const ACTORS_TABLE_NAME: &str = "actors";
+
+struct Actor<'a> {
+    id: u32,
+    name: &'a str,
+}
 
 fn main() -> Result<(), String> {
     let conn = create_databases()?;
@@ -20,7 +24,7 @@ fn main() -> Result<(), String> {
 fn create_databases() -> Result<Connection, String> {
     let conn = Connection::open(DATABASE_NAME)
         .map_err(|_| format!("Failed to open database {DATABASE_NAME}"))?;
-    conn.execute(format!("CREATE TABLE IF NOT EXISTS {ACTORS_TABLE_NAME} ( id integer, name text not null)"))
+    conn.execute(format!("CREATE TABLE IF NOT EXISTS {ACTORS_TABLE_NAME} ( id integer primary key, name text not null)"))
         .map_err(|_| String::from("Could not create table for actor names"))?;
 
     Ok(conn)
@@ -30,12 +34,14 @@ fn fill_names_database(conn: &Connection) -> Result<(), String> {
     let names =
         File::open(ACTORS_TSV_FILE).map_err(|_| String::from("Failed to read name.basics.tsv"))?;
     let reader = BufReader::new(names);
-    reader.lines().skip(1).map_while(Result::ok).for_each(|l| {
+    reader.lines().skip(1).map_while(Result::ok).map(|l| {
         let values: Vec<&str> = l.split('\t').collect();
         let id: u32 = values.first().unwrap()[2..].parse().unwrap();
         let name = values.get(1).unwrap();
-        conn.execute(format!("INSERT INTO {ACTORS_TABLE_NAME} VALUES({id}, {name}"))
-            .map_err(|_| format!("Could not insert values {id}, {name} into {ACTORS_TABLE_NAME}")).unwrap();
+        conn.execute(format!(
+            "INSERT INTO {ACTORS_TABLE_NAME} VALUES({id}, \"{name}\")"
+        ))
+        .unwrap();
     });
 
     Ok(())
