283a89468cd39518e352cee0b250670f0bd154cc
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-05 12:41:14 +0200

    Add printing in names

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use std::{fs::File, io::{self, BufReader, Error}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+};
 
 fn main() -> Result<(), Error> {
-    let names = File::open("./dictionary.json")?;
-    let reader = BufReader::new(dictionary_file);
+    let names = File::open("../name.basics.tsv")?;
+    let reader = BufReader::new(names);
+    reader
+        .lines()
+        .map_while(Result::ok)
+        .flat_map(|l| l.split('\t').map(str::to_owned).collect::<Vec<_>>()).for_each(|l| println!("{l}"));
 
     println!("Finished Converting.");
     Ok(())
