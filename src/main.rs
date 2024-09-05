84cb2d0d826923471dfef4f17ae57931eb597369
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-05 13:02:18 +0200

    Add creating databases

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
+use sqlite::Connection;
 use std::{
     fs::File,
-    io::{BufRead, BufReader, Error},
+    io::{BufRead, BufReader},
 };
 
-fn main() -> Result<(), Error> {
-    let names = File::open("name.basics.tsv")?;
+fn main() -> Result<(), String> {
+    let conn = create_databases()?;
+    let names =
+        File::open("name.basics.tsv").map_err(|_| String::from("Failed to read names database"))?;
     let reader = BufReader::new(names);
     reader.lines().map_while(Result::ok).for_each(|l| {
-        for (i, v) in l.split('\t').enumerate() {
-
-        };
+        for (i, v) in l.split('\t').enumerate() {}
     });
 
     println!("Finished Converting.");
     Ok(())
+}
+
+fn create_databases() -> Result<Connection, String> {
+    let conn = Connection::open("names.db")
+        .map_err(|_| String::from("Failed to open database names.db"))?;
+    conn.execute("create table if not exists actors ( id integer primary key, name text not null)")
+        .map_err(|_| String::from("Could not create table"))?;
+
+    Ok(conn)
 }
