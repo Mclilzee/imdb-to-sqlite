98f21c644da22670c2e152b5319d2455cd12c575
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-08 00:50:54 +0200

    Add parsing titles

--- src/title.rs
+++ src/title.rs
@@ -1,3 +1,7 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
+const TITLE_TSV_FILE: &str = "title.basics.tsv";
+
 #[derive(Debug)]
 pub struct Title {
     pub id: u32,
@@ -10,28 +14,23 @@ pub struct Title {
 }
 
 impl Title {
-   //2   │ tt0000001   short   Carmencita  Carmencita  0   1894    \N  1   Documentary,Short
     fn from(line: String) -> Result<Self, String> {
         let values: Vec<&str> = line.split('\t').collect();
         let id: u32 = values.first().unwrap()[2..].parse().unwrap();
-        let title_type = 
-        let primary_name = values.get(1).map(|&s| s.to_string()).unwrap();
-        let original_name = values.get(2).map(|&s| s.to_string()).unwrap();
-        let birth_date = values.get(2).and_then(|v| v.parse::<u16>().ok());
-        let death_date = values.get(3).and_then(|v| v.parse::<u16>().ok());
-        let professions = values
+        let title_type = values.get(1).map(|&s| s.to_string()).unwrap();
+        let primary_name = values.get(2).map(|&s| s.to_string()).unwrap();
+        let original_name = values.get(3).map(|&s| s.to_string()).unwrap();
+
+        let release_date = values.get(5).and_then(|v| v.parse::<u16>().ok());
+        let end_date = values.get(6).and_then(|v| v.parse::<u16>().ok());
+        let genres = values
             .get(4)
             .map(|&v| v.split(',').map(|v| v.to_string()).collect::<Vec<_>>())
             .unwrap();
 
-        let titles = values
-            .get(5)
-            .map(|v| v.split(','))
-            .map(|v| v.flat_map(|n| n[2..].parse::<u32>()).collect::<Vec<_>>())
-            .unwrap();
-
-        Ok(Title {
+        Ok(Self {
             id,
+            title_type,
             primary_name,
             original_name,
             release_date,
@@ -41,15 +40,15 @@ impl Title {
     }
 }
 
-pub fn get_actors() -> Result<Vec<Actor>, String> {
-    let names = File::open(ACTORS_TSV_FILE)
-        .map_err(|e| format!("Unable to read from {ACTORS_TSV_FILE} -> {e}"))?;
+pub fn get_titles() -> Result<Vec<Title>, String> {
+    let names = File::open(TITLE_TSV_FILE)
+        .map_err(|e| format!("Unable to read from {TITLE_TSV_FILE} -> {e}"))?;
 
     BufReader::new(names)
         .lines()
         .skip(1)
         .map(|l| l.map_err(|e| format!("Unable to read line -> {e}")))
-        .map(|l| l.and_then(Actor::from))
+        .map(|l| l.and_then(Title::from))
         .collect()
 }
 
