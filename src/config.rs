9acdf4d8919b509d1d8706d52ad0de7dc74288d3
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-13 10:42:59 +0200

    Add more options

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,12 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = "Parse and convert imdb TSV (Tab Saparated Values) Into a Sqlite table")]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "Parse and convert imdb TSV (Tab Saparated Values) Into a Sqlite table"
+)]
 pub struct Args {
     /// File name of the database, if file doesn't exist, then file will be created
     pub path: String,
@@ -10,11 +15,19 @@ pub struct Args {
     #[arg(short = 'O', long = "overwrite")]
     pub overwrite: bool,
 
-    /// Lite option will create the core tables and the one joining table between them (title, name, name_title)
+    /// Lite option will toggle the core tables and the one joining table between them (title, name, name_title)
     #[arg(short = 'l', long = "lite")]
     pub lite: bool,
 
-    /// Core option will make sure to convert the 2 core tables, (title, name)
+    /// Core option will toggle the two core tables, (title, name)
     #[arg(short = 'c', long = "core")]
     pub core: bool,
+
+    /// Full option will toggle all the tables except the slowest ones, (title_principal, title_character, title_locale)
+    #[arg(short = 'f', long = "full")]
+    pub full: bool,
+
+    /// Extra option will toggle the extra tables which are the slowest and will take a long time to parse, (title_principal, title_character, title_locale)
+    #[arg(short = 'e', long = "extra")]
+    pub extra: bool,
 }
