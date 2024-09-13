4de5f6f816524b3d7df420cf6183cf96c1695218
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-13 10:40:29 +0200

    Add more options

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,15 @@ pub struct Args {
     /// File name of the database, if file doesn't exist, then file will be created
     pub path: String,
 
-    /// Overwrite option will erase the content of File if it exists and write it from the beggining
-    #[arg(short = 'o', long = "overwrite")]
+    /// Overwrite option will erase all the content in the file. Only choose it if you want to do a fresh conversion otherwise all the old ones will be replaced.
+    #[arg(short = 'O', long = "overwrite")]
     pub overwrite: bool,
+
+    /// Lite option will create the core tables and the one joining table between them (title, name, name_title)
+    #[arg(short = 'l', long = "lite")]
+    pub lite: bool,
+
+    /// Core option will make sure to convert the 2 core tables, (title, name)
+    #[arg(short = 'c', long = "core")]
+    pub core: bool,
 }
